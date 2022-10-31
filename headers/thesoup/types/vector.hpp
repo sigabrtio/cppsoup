016923d3dd0a9de0ae4420f8bbec220b36b6f36e
Amartya Datta Gupta <adattagupta@twitter.com>
2022-10-31 14:45:36 -0700

    Completed initial implementation of list vector.

--- headers/thesoup/types/vector.hpp
+++ headers/thesoup/types/vector.hpp
@@ -53,6 +53,8 @@ namespace thesoup {
                 other.pages.clear();
                 other._size = 0;
             }
+            Vector<T, page_size>& operator*() {return *this;}
+            Vector<T, page_size>* operator->() {return this;}
 
             void push_back(const T& item) noexcept {
                 std::size_t page_number_to_insert_to {(_size)/num_items_per_page};
@@ -77,6 +79,43 @@ namespace thesoup {
             std::size_t bytes() const noexcept {
                 return pages.size() * page_size;
             }
+
+            struct Iterator {
+                using iterator_category [[maybe_unused]] = std::random_access_iterator_tag;
+                using difference_type   = std::ptrdiff_t;
+                using value_type [[maybe_unused]] = T;
+                using pointer           = T*;
+                using reference         = T&;
+
+                Vector<T, page_size>* enclosing;
+                std::size_t idx {};
+
+                Iterator(
+                        Vector<T, page_size>* enclosing,
+                        const std::size_t& idx) : enclosing {enclosing}, idx {idx} {}
+
+                reference operator*() noexcept {return (*enclosing)[idx];}
+                pointer operator->() noexcept {return &(*enclosing)[idx];}
+
+                Iterator& operator++() noexcept { idx++; return *this;}
+                Iterator operator++(int) noexcept { Iterator tmp {enclosing, idx}; idx++; return tmp;}
+
+                Iterator& operator--() noexcept { idx--; return *this;}
+                Iterator operator--(int) noexcept { Iterator tmp {enclosing, idx}; idx--; return tmp;}
+
+                Iterator operator+(const std::size_t& val) noexcept {return Iterator {enclosing, idx+val};}
+                Iterator operator-(const std::size_t& val) noexcept {return Iterator {enclosing, idx-val};}
+
+                difference_type operator-(const Iterator& other) noexcept {return other.idx - idx;}
+
+                Iterator& operator+=(const std::size_t& val) noexcept {idx+=val; return *this;}
+                Iterator& operator-=(const std::size_t& val) noexcept {idx-=val; return *this;}
+                bool operator== (const Iterator& other) const noexcept { return enclosing == other.enclosing && idx == other.idx; };
+                bool operator!= (const Iterator& other) const noexcept { return enclosing != other.enclosing || idx != other.idx; };
+            };
+
+            Iterator begin() noexcept {return Iterator(this, 0);}
+            Iterator end() noexcept {return Iterator(this, _size);}
         };
     }
 }
--- tst/thesoup/types/vector.cc
+++ tst/thesoup/types/vector.cc
@@ -1,5 +1,6 @@
 #define CATCH_CONFIG_MAIN
 
+#include <iterator>
 #include <thesoup/types/vector.hpp>
 #include <catch2/catch_all.hpp>
 
@@ -53,6 +54,90 @@ SCENARIO("Vector happy case.") {
                     }
                 }
             }
+
+            AND_WHEN("I construct another vector from this one via a move.") {
+
+                auto other_vec {std::move(my_vec)};
+
+                THEN("The other vector should be properly initialized.") {
+
+                    REQUIRE(Weird3byteStruct{'a','b','c'} == other_vec[0]);
+                    REQUIRE(1 == other_vec.size());
+                    REQUIRE(4 == other_vec.bytes());
+
+                    AND_THEN("The original vector should have been reset.") {
+
+                        REQUIRE(0 == my_vec.size());
+                        REQUIRE(0 == my_vec.bytes());
+                    }
+                }
+            }
+
+            AND_WHEN("I assign another vector from this one via a move.") {
+
+                Vector<Weird3byteStruct, 4> other_vec {};
+                other_vec = std::move(my_vec);
+
+                THEN("The other vector should be properly initialized.") {
+
+                    REQUIRE(Weird3byteStruct{'a','b','c'} == other_vec[0]);
+                    REQUIRE(1 == other_vec.size());
+                    REQUIRE(4 == other_vec.bytes());
+
+                    AND_THEN("The original vector should have been reset.") {
+
+                        REQUIRE(0 == my_vec.size());
+                        REQUIRE(0 == my_vec.bytes());
+                    }
+                }
+            }
+        }
+    }
+}
+
+SCENARIO("Vector iterations.") {
+
+    GIVEN("I have a vector with some elements in it.") {
+
+        Vector<Weird3byteStruct, 4> my_vec;
+        my_vec.push_back({'a', 'b', 'c'});
+        my_vec.push_back({'a', 'b', 'd'});
+        my_vec.push_back({'a', 'b', 'e'});
+        my_vec.push_back({'a', 'b', 'f'});
+        my_vec.push_back({'a', 'b', 'g'});
+
+        WHEN("I iterate through it.") {
+
+            auto it {my_vec.begin()};
+
+            THEN("The iterator operations should work as expected.") {
+
+                REQUIRE(Weird3byteStruct{'a', 'b', 'c'} == *it);
+                it++;
+                REQUIRE(Weird3byteStruct{'a', 'b', 'd'} == *it);
+                it++;
+                REQUIRE(Weird3byteStruct{'a', 'b', 'e'} == *it);
+                it++;
+                REQUIRE(Weird3byteStruct{'a', 'b', 'f'} == *it);
+                it--;
+                REQUIRE(Weird3byteStruct{'a', 'b', 'e'} == *it);
+                it += 2;
+                REQUIRE(Weird3byteStruct{'a', 'b', 'g'} == *it);
+                it -= 2;
+                REQUIRE(Weird3byteStruct{'a', 'b', 'e'} == *it);
+
+                std::size_t acc {};
+                for ([[maybe_unused]] auto& _ : my_vec) {
+                    acc++;
+                }
+                REQUIRE(my_vec.size() == acc);
+            }
+
+            THEN("The distance functions should work.") {
+
+                REQUIRE(static_cast<long>(my_vec.size()) == std::distance(my_vec.end(), my_vec.begin()));
+            }
+
         }
     }
 }
